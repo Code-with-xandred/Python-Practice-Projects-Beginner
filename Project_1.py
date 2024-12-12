a5e4f9ff87f2e792559132696ca2ca9be6615b3d
Code-with-xandred <stak55795@gmail.com>
2024-12-12 16:42:50 +0530

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,34 @@
+#1.Python Calculator
+
+print("Select Operator \n" \
+      "1.Addition \n" \
+      "2.Substraction \n" \
+      "3.Multiplication \n" \
+      "4.Division \n" \
+      "5.Average")
+
+user_Input = int(input("Select Operator (1, 2, 3, 4, 5): "))
+
+num1 = int(input("Enter The First Number:- "))
+num2 = int(input("Enter The Second Number:- "))
+
+if user_Input == 1:
+    result = num1 + num2
+    print(f"The Answer Is: {result}")
+elif user_Input == 2:
+    result = num1 - num2
+    print(f"The Answer Is: {result}")
+elif user_Input == 3:
+    result = num1 * num2
+    print(f"The Answer Is: {result}")
+elif user_Input == 4:
+    result = num1 / num2
+    print(f"The Answer Is: {result}")
+elif user_Input == 5:
+    result = num1 + num2 /2
+    print(f"The Answer IS: {result}")
+else:
+    print(None)
+
+# ______________________________________________________________________
+
